e8a1b8ea179828dcaf37f6a3c612bf68cc0c84bf
Shruti Chauhan <shrutichauhanddc@gmail.com>
2024-02-21 17:48:26 +0530

    commit

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+public class BinarySearch {
+public static int BinarySearchfunction(int number[],int key){
+        int start=0;
+        int end=number.length-1;
+        
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(number[mid]==key){
+                return mid;
+            }else if(number[mid]<key){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+            
+        }
+
+
+
+return -1;
+
+}
+
+
+
+
+
+    public static void main(String[] args) {
+        int number[]={4,6,33,67,81,99,400,501,694,800};
+        int key=400;
+        System.out.println("the index is ="+ BinarySearchfunction(number,key));
+
+    }
+    
+}
