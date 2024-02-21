09d8422b8452c6786e5853e3a2b9833a2f733b1b
Shruti Chauhan <shrutichauhanddc@gmail.com>
2024-02-21 17:57:21 +0530

    commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -20,7 +20,7 @@ return -1;
  public static void main(String[] args) {
         int number[]={4,6,33,67,81,99,400,501,694,800};
         int key=400;
-        System.out.println("the index is of the given number  ="+ BinarySearchfunction(number,key));
+        System.out.println("the index  of the given number  ="+ BinarySearchfunction(number,key));
 
     }
     
