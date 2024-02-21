02c6745c866838ec0d3502cf5e14204be10b8399
Shruti Chauhan <shrutichauhanddc@gmail.com>
2024-02-21 23:04:23 +0530

    commit

---
+++ Arraypair.java
@@ -0,0 +1,14 @@
+public class Arraypair {
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        for(int i=0;i<arr.length;i++){
+            for( int j=i+1;j<arr.length;j++){
+            // while((i+1) < arr.length){
+                System.out.print("("+arr[i]+","+ arr[j]+")");
+            // }
+        }
+            
+        }
+    }
+    
+}
