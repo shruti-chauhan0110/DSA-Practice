f7c80eb6b5e202107d570a26dcb5c12051baaa44
Shruti Chauhan <shrutichauhanddc@gmail.com>
2024-02-27 00:12:46 +0530

    commit

--- FindElement2DArray.java
+++ FindElement2DArray.java
@@ -1,7 +1,19 @@
 import java.util.*;
 public class FindElement2DArray {
-    public static void main(String[] args) {
+    public static boolean search(int matrix[][],int key){
+        for(int i=0;i < matrix.length;i++){
+            for(int j=0;j< matrix[0].length;j++){
+                if(matrix[i][j]==key){
+                    System.out.println("element is found at=" + i+","+j);
+                }
+            }
+        }
+        return false;
         
+
+    }
+    public static void main(String[] args) {
+       
         int matrix[][]=new int[3][3];
         Scanner sc=new Scanner(System.in);
         for(int i=0;i < matrix.length;i++){
@@ -15,7 +27,10 @@ public class FindElement2DArray {
             }
             System.out.println();
         }
+        System.out.print("enter the element to find it in the matrix=");
 
+        int a=sc.nextInt();
+        search(matrix, a);
 
     }
     
